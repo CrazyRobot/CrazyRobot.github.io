fd29fe5dfa35a21d31cd1bd087640e33bdfc44ec
haungle <huangle@szewec.com>
2025-08-19 16:49:17 +0800

    更新文档

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -50,7 +50,7 @@ export default defineThemeConfig({
    */
   profile: {
     avatar: 'https://theme-plume.vuejs.press/plume.png',
-    name: 'My Vuepress Site',
+    name: 'My Blogs',
     description: '',
     // circle: true,
     // location: '',
