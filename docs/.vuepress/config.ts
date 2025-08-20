6a42def7d2d0be8af82d7aeec7a3445b6a2410d7
haungle <huangle@szewec.com>
2025-08-20 09:53:57 +0800

    更新文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,7 +29,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    hostname: 'https://github.com/CrazyRobot/',
+    // hostname: 'https://your_site_url',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
@@ -39,7 +39,7 @@ export default defineUserConfig({
     /* 页内信息 */
     // editLink: true,
     // lastUpdated: true,
-    // contributors: true,
+    contributors: false,
     // changelog: false,
 
     /**
